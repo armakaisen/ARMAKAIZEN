9c5613c6f321a5860eb91a7cd216c93cce7ecbe6
unknown <armakaisen@gmail.com>
2020-04-09 19:50:44 -0400

    subido desde notebook

---
+++ src/lib/helpers.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+const helpers = {};
+
+helpers.encryptPassword = async (password)=>{
+    const salt = await bcrypt.genSalt(10);        // saltos de encriptacion de seguridad 10 veces (puede ser mas)
+    const passwordhash = await bcrypt.hash(password, salt);            // passwordhash ya tiene la clave cifrada encriptada
+    return passwordhash;
+}
+
+helpers.comparePassword = async (password, passwordguardado)=>{
+    try {
+        return await bcrypt.compare(password, passwordguardado);
+    } catch (error) {
+        console.log(error);
+    }
+}
+module.exports = helpers;
---
+++ src/routes/links.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const ruta = express.Router();
+const helpers = require('../lib/helpers');
+const db_pool = require('../database');
+const {isLoggedIn, isNotLoggedIn} = require('../lib/autoriza');
+
+ruta.get('/add', isLoggedIn, (req, res)=>{
+    res.render('links/add');
+})
+
+ruta.post('/add', async (req, res)=>{
+    const {title, url, description} = req.body;
+    var lcTitle = req.body.title || '';
+    var lcURL = req.body.url || '';
+    if (lcTitle == ''){
+        req.flash('message', 'Debe introducir título');
+        //console.log(req.body);
+        res.redirect('/add');
+    }else {
+        if (lcURL == ''){
+            req.flash('message', 'Debe introducir URL');
+            //res.body.url.focus();
+           
+            res.redirect('/add');
+        }else {
+            const newLink = {   
+                title,
+                url,
+                description
+            }
+            //console.log(newLink);   
+            await db_pool.query('INSERT INTO links set ?', [newLink]);
+            req.flash('success', 'Guardado correctamente');
+            res.redirect('/list');
+        }
+    }
+});
+
+/*
+ruta.get('/',  (req, res)=>{
+    res.redirect('/signin');
+})*/
+ 
+
+ruta.get('/list', isLoggedIn, async (req, res)=>{
+    const links = await db_pool.query('SELECT * FROM vw_links');
+    res.render('links/list', {links});
+    //console.log(links);
+    //res.send('aqui saldrá');
+})
+
+ruta.get('/delete/:id', isLoggedIn, async (req, res)=>{
+    //console.log(req.params.id);
+    //res.send('eliminado');
+    const { id } = req.params;
+    await db_pool.query('DELETE FROM links WHERE id = ?', [id]);
+    req.flash('success','Registro eliminado...');
+    res.redirect('/list');
+})
+
+ruta.get('/edit/:id', isLoggedIn, async (req,res)=>{
+    const { id } = req.params;
+    //console.log(id);
+    const link = await db_pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    //console.log(link[0]);
+    res.render('links/edit', {link: link[0]});
+})
+
+ruta.post('/edit/:id', async (req,res)=>{
+    const { id } = req.params;    
+    const { title, url, description } = req.body;
+    const updLink = {
+        title,
+        url,
+        description
+    };
+
+    const link = await db_pool.query('UPDATE links set ? WHERE id = ?', [updLink, id]);
+    req.flash('success', 'Registro fue actualizado...');
+    res.redirect('/list');
+})
+
+/* USUARIOS */
+ruta.get('/lstusuarios', isLoggedIn, async (req, res)=>{
+    const tblusuarios = await db_pool.query('SELECT * FROM usuarios');
+    //console.log(tblusuarios);
+    res.render('links/lstusuarios', {tblusuarios});
+})
+
+
+ruta.get('/edtusuario/:id_usuario', isLoggedIn, async (req,res)=>{
+    const { id_usuario } = req.params;
+    //onsole.log(id_usuario);
+    const tblusuario = await db_pool.query('SELECT * FROM usuarios WHERE id_usuario = ?', [id_usuario]);
+    //console.log(tblusuario[0]);
+    res.render('links/edtusuario', {usuario: tblusuario[0]});
+})
+
+ruta.post('/edtusuario/:id_usuario', async (req,res)=>{
+    const { id } = req.params;    
+    const { fullname, username, password } = req.body;
+    const updUsuario = {
+        login: username,
+        clave: password,
+        nombre: fullname
+    };
+    updUsuario.clave = await helpers.encryptPassword(password);
+
+    const link = await db_pool.query('UPDATE usuarios set ? WHERE id_usuario = ?', [updUsuario, id]);
+    req.flash('success', 'Registro fue actualizado...');
+    res.redirect('/lstusuarios');
+})
+
+ruta.get('/dltusuario/:id_usuario', isLoggedIn, async (req, res)=>{
+    //console.log(req.params.id);
+    //res.send('eliminado');
+    const { id } = req.params;
+    try {
+        await db_pool.query('DELETE FROM usuarios WHERE id = ?', [id]);
+        req.flash('success','Registro eliminado...');         
+    } catch (error) {
+        req.flash('message', 'Hubo un error al intentar borrar el registro...');
+    }
+       res.redirect('/lstusuarios');
+})
+module.exports = ruta;
